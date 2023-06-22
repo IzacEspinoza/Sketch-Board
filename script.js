da2a72763ba9c439ee09ce3929c2b630b3b8c228
Izac <izikki11499@gmail.com>
2023-06-21 17:31:05 -0700

    Edit to styling Added a title to center of the screen, refined some divs and spacings, also declared a deafult grid size

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //get our grid
 const grid = document.querySelector('.grid');
+//DEFAULT GRID SIZE
+const DEFAULT_GRID = 256;
 //Grid generator function
-makeGrid = () =>{
+makeGrid = (number) =>{
     //for loop to create the divs to fill up the grid space(16x16 = 256)
-    for(let i = 0; i < 256; i++){
+    for(let i = 0; i < number; i++){
         //create div while loop is active
         const div = document.createElement('div');
         //add class to the divs
@@ -12,12 +14,12 @@ makeGrid = () =>{
 
         //add event listeners to each div cell created(to allow drawing effect)
         div.addEventListener('mouseover', function(event){
-            event.target.style.backgroundColor = 'blue';
+            event.target.style.backgroundColor = 'red';
         })
         //append the divs to the grid container to make the grid squares
         grid.appendChild(div);
     }
 
 }
-//Call the grid
-makeGrid();
+//Call the grid(default size)
+makeGrid(DEFAULT_GRID);

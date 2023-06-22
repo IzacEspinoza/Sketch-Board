35994161402cada56e0ea5a1acdd8178771ba849
Izac <izikki11499@gmail.com>
2023-06-21 18:06:03 -0700

    made cler/erase function, removed bgc from css(no need) Created a clear grid function that resets the grids color to default white, and removed some obsolete background color line in .grid{}, now just need to add button to click for the clear()

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 //get our grid
-const grid = document.querySelector('.grid');
+const grid = document.querySelector(".grid");
+//get our grid cells
+const grid_cells = grid.childNodes;
 //DEFAULT GRID SIZE
 const DEFAULT_GRID = 256;
+
 //Grid generator function
-makeGrid = (number) =>{
-    //for loop to create the divs to fill up the grid space(16x16 = 256)
-    for(let i = 0; i < number; i++){
-        //create div while loop is active
-        const div = document.createElement('div');
-        //add class to the divs
-        div.classList.add('grid-cell');
-        //forEach to add eventListeners to each div
+makeGrid = (number) => {
+  //for loop to create the divs to fill up the grid space(16x16 = 256)
+  for (let i = 0; i < number; i++) {
+    //create div while loop is active
+    const div = document.createElement("div");
+    //add class to the divs
+    div.classList.add("grid-cell");
+    //forEach to add eventListeners to each div
 
-        //add event listeners to each div cell created(to allow drawing effect)
-        div.addEventListener('mouseover', function(event){
-            event.target.style.backgroundColor = 'red';
-        })
-        //append the divs to the grid container to make the grid squares
-        grid.appendChild(div);
-    }
+    //add event listeners to each div cell created(to allow drawing effect)
+    div.addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = "red";
+    });
+    //append the divs to the grid container to make the grid squares
+    grid.appendChild(div);
+  }
+};
 
-}
+//Clear?
+const clearGrid = () => {
+  // grid_cells.forEach(cell => console.log(cell));
+  grid_cells.forEach((cell) => (cell.style.backgroundColor = "white"));
+};
+
 //Call the grid(default size)
 makeGrid(DEFAULT_GRID);
